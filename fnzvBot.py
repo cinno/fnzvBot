8962d322308c2d6796c3b1ecff29b321ea5a8be9
Yessou Sami <yessou.sami@gmail.com>
2015-11-11 19:45:34 +0100

    Update fnzvBot.py

--- fnzvBot.py
+++ fnzvBot.py
@@ -1,4 +1,6 @@
 # -*- coding: cp1252 -*-
+# HTTP Botnet with control Panel 
+
 #This program is distributed in the hope that it will be useful,
 #but WITHOUT ANY WARRANTY; without even the implied warranty of
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
